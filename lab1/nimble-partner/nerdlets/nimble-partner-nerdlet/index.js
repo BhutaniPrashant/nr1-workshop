2f740b3e91d805ec7197c65e4aba55936d12f907
Prashant Bhutani <33264208+BhutaniPrashant@users.noreply.github.com>
2020-02-18 11:58:59 -0600

    .

---
+++ lab1/nimble-partner/nerdlets/nimble-partner-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class NimblePartnerNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, nimble-partner-nerdlet Nerdlet!</h1>;
+    }
+}
